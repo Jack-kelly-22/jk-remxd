f61c5742c35b5d72c52994d2b09a4dfbba9b88e4
Jack-kelly-22 <jacklaxjk@gmail.com>
2022-06-04 20:02:00 -0700

    update routes

--- app/routes/IRL.tsx
+++ app/routes/IRL.tsx
@@ -1,9 +1,27 @@
 import { ethers } from 'ethers';
 import { useState,useRef } from 'react';
 
+
+
 import { VncScreen } from 'react-vnc';
 
+const OptoABI = [
+  // "function balanceOf(address) view returns (uint256)",
+  "function tokenURI(uint256) view returns (string)",
+]
+
+const OPTO_ADDRESS = "0xd38a0D565F1b981f2736528cd071239432c2fdc1";
+
 const BASE_URL = "47.45.119.117";
+
+const getOptos = async (provider) => {
+  const optoC = new ethers.Contract(OPTO_ADDRESS, OptoABI, provider);
+  // const optos = await optoC.balanceOf(provider.address);
+  const baseURI = await optoC.tokenURI(0);
+  console.log(optos);
+  return optos;
+  // const 
+}
 
 export default function IRL() {
     const connectToMetamask = async () => {
@@ -11,26 +29,25 @@ export default function IRL() {
         const provider = new ethers.providers.Web3Provider(window.ethereum)
         const accounts = await provider.send("eth_requestAccounts", []);
         const balance = await provider.getBalance(accounts[0]);
+        // const optos = await getOptos(provider);
         const balanceInEther = ethers.utils.formatEther(balance);
         const block = await provider.getBlockNumber();
         setWalletConnected(true);
-        // setProvider(provider);
+        // setOptos(optos);
         setWalletAddress(accounts[0]);
-        // getPool(provider).then(data => {
-          // console.log("pool data",data);
-        //   setPoolData(data);
-          
-        // });
-        // setWalletBalance(balanceInEther);
+       
         }
       }
+    const [optos, setOptos] = useState(null);
     const [walletConnected, setWalletConnected] = useState(false);
     const [walletAddress, setWalletAddress] = useState("");
     const vncScreenRef = useRef<React.ElementRef<typeof VncScreen>>(null);
   return (
     <div className='flex flex-col items-center p-3 m-3'>
       <div className="mockup-code">
-        <pre data-prefix="$"><code> Welcome to </code></pre> 
+        <pre data-prefix="$"><code> Welcome to aptosXoptos</code></pre> 
+        <pre data-prefix=">>>" data-suffix=">>>"><code>Your opto hosts</code>  </pre>
+        
         {/* <pre data-prefix=">" className="text-warning"><code>installing...</code></pre>  */}
         <pre data-prefix=">" className="text-success"><code>Done!</code></pre>
         </div>
@@ -38,7 +55,7 @@ export default function IRL() {
       {walletConnected ? (
         <div>
             <h2 className='text-green text-2xl text left m-2'>Wallet Connected</h2>
-            <p>Your hosts: </p>
+            <p>Your hosts: {optos}</p>
             {/* <h3>Balance: {walletAddress}</h3> */}
         </div>) : ( <div className='p-3 m-2'>
             <h2>Wallet Not Connected</h2>
@@ -55,7 +72,7 @@ export default function IRL() {
       ref={vncScreenRef}
     /> */}
     {walletConnected ? (
-      <iframe title="host" src={`http://opto0:6081/vnc.html?host=opto0&port=6081`} width='1000px' height="800px" />
+      <iframe title="host" src={`http://opto0:6082/vnc.html?host=opto0&port=6082`} width='1000px' height="800px" />
     ) : (
       "no Hosts available")}
       
